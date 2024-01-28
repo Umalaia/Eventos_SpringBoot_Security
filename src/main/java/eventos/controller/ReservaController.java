d994ec832e2a7171d94c3dc6fdb312bf5c468d47
Juanjo <sistemasjuanjo@outlook.es>
2024-01-28 16:41:36 +0100

    Agrego comentarios a los metodos

--- src/main/java/eventos/controller/ReservaController.java
+++ src/main/java/eventos/controller/ReservaController.java
@@ -33,7 +33,18 @@ public class ReservaController {
 	@Autowired
 	private EventoDao eDao;
 	
-	
+	/**
+	 * Este método maneja solicitudes GET para mostrar las reservas de un usuario. Verifica si hay un usuario autenticado, 
+	 * obtiene la información del usuario y sus reservas asociadas, y luego pasa esta información al modelo antes de redirigir al usuario
+	 * a la vista "misReservas". Si no hay un usuario autenticado, redirige al usuario a la página de inicio de sesión.
+	 * 
+	 * @param model			Para pasar datos a la vista
+	 * @param aut			Objeto para manejar la información de autenticación del usuario
+	 * @param misesion		Objeto para controlar la sesión.
+	 * @param usuario		Objeto usuario con la información del mismo.
+	 * @return				Devuelve el nombre de la vista que se mostrará después de ejecutar este método "misReservas".
+	 * 						En caso de que el usuario no este autentificado redirige a la página ("/login").
+	 */
 	@GetMapping("/misReservas")
 	public String verMisReservas(Model model, Authentication aut, HttpSession misesion, Usuario usuario) {
 	    if (aut != null && aut.isAuthenticated()) {
@@ -47,7 +58,15 @@ public class ReservaController {
 	        return "redirect:/login";
 	    }
 	}
-	
+	/**
+	 * Este método maneja solicitudes GET para eliminar una reserva específica. Intenta eliminar la reserva con el identificador proporcionado, 
+	 * agrega un mensaje al modelo según el resultado y redirige al usuario de vuelta a la página de sus reservas.
+	 * 
+	 * @param idReserva		Identificador de la reserva a eliminar
+	 * @param model			Para pasar datos a la vista
+	 * @return				Redirige al usuario a la página de sus reservas después de eliminar la reserva.
+	 * 						En caso de no poder eliminar la reserva, lo indica con un mensaje ("No ha podido eliminar la reserva").
+	 */
 	@GetMapping("/misReservas/eliminar/{id}")
 	public String eliminarReserva(@PathVariable ("id") int idReserva, Model model) {
 		if(rDao.eliminarReserva(idReserva) == 1) {
