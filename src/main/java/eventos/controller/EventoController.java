00f6990c34a6c355ef995cc7e4a8871c8620fcb2
sheila.orejuela <she.or.va@gmail.com>
2024-01-29 19:00:24 +0100

    formateo, falta documentar

--- src/main/java/eventos/controller/EventoController.java
+++ src/main/java/eventos/controller/EventoController.java
@@ -1,6 +1,5 @@
 package eventos.controller;
 
-
 import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,161 +26,154 @@ public class EventoController {
 
 	@Autowired
 	private EventoDao eDao;
-	
+
 	@Autowired
 	private UsuarioDao uDao;
-	
+
 	@Autowired
 	private ReservaDao rDao;
-	
+
 	/**
-	 * Este método ver detalles, muestra una vista con el evento específico y el usuario.
+	 * Este método ver detalles, muestra una vista con el evento específico y el
+	 * usuario.
 	 * 
-	 * @param idEvento		Contiene el valor númerico de la variable asignada en la url
-	 * @param model			Es un objeto que se utiliza para pasar información entre el controlador y la vista.
-	 * @param usuario		Objeto, el cual esta realizando la solicitud
-	 * @return				Devolverá la vista "detalles" utilizando la información proporcionada en el modelo
+	 * @param idEvento Contiene el valor númerico de la variable asignada en la url
+	 * @param model    Es un objeto que se utiliza para pasar información entre el
+	 *                 controlador y la vista.
+	 * @param usuario  Objeto, el cual esta realizando la solicitud
+	 * @return Devolverá la vista "detalles" utilizando la información proporcionada
+	 *         en el modelo
 	 */
-	
+
 	@GetMapping("/detalles/{id}")
-	public String detallesEventos (@PathVariable("id") int idEvento, Model model,Authentication auth, Reserva reserva) {
-	    Evento evento = eDao.verUnEvento(idEvento);
-	    auth = SecurityContextHolder.getContext().getAuthentication();
-        String usu = auth.getName();
+	public String detallesEventos(@PathVariable("id") int idEvento, Model model, Authentication auth, Reserva reserva) {
+		Evento evento = eDao.verUnEvento(idEvento);
+		auth = SecurityContextHolder.getContext().getAuthentication();
+		String usu = auth.getName();
 
-	    int cantidadDisponible = evento.getAforoMaximo() - rDao.cantReservas(idEvento);
-	    int limiteMaximo = 10 - rDao.rvasPorUsuarioYEvento(idEvento, usu);
+		int cantidadDisponible = evento.getAforoMaximo() - rDao.cantReservas(idEvento);
+		int limiteMaximo = 10 - rDao.rvasPorUsuarioYEvento(idEvento, usu);
 
-	    model.addAttribute("evento", evento);
-	    model.addAttribute("usuario", usu);
-	    model.addAttribute("cantidad", cantidadDisponible);
-	    model.addAttribute("limiteMaximo", limiteMaximo);
+		model.addAttribute("evento", evento);
+		model.addAttribute("usuario", usu);
+		model.addAttribute("cantidad", cantidadDisponible);
+		model.addAttribute("limiteMaximo", limiteMaximo);
 
-	    return "detalles";
+		return "detalles";
 	}
 
-	
-	
 	@PostMapping("/detalles/{id}")
-    public String realizarReserva(@PathVariable ("id") int idEvento, @RequestParam int cantidad, @RequestParam String observaciones, Authentication auth) {
-        auth = SecurityContextHolder.getContext().getAuthentication();
-        String username = auth.getName();
-        Evento evento = eDao.verUnEvento(idEvento);
-        
-        BigDecimal precioVenta =  evento.getPrecio().multiply(BigDecimal.valueOf(cantidad));
-        Reserva reserva = new Reserva(cantidad, observaciones, precioVenta, evento, uDao.verUsuario(username));
-        
-        rDao.realizarReserva(reserva);
-        return "redirect:/misReservas";
-    }
-	
-	
-	
-	
-	
-	
-	
+	public String realizarReserva(@PathVariable("id") int idEvento, @RequestParam int cantidad,
+			@RequestParam String observaciones, Authentication auth) {
+		auth = SecurityContextHolder.getContext().getAuthentication();
+		String username = auth.getName();
+		Evento evento = eDao.verUnEvento(idEvento);
+
+		BigDecimal precioVenta = evento.getPrecio().multiply(BigDecimal.valueOf(cantidad));
+		Reserva reserva = new Reserva(cantidad, observaciones, precioVenta, evento, uDao.verUsuario(username));
+
+		rDao.realizarReserva(reserva);
+		return "redirect:/misReservas";
+	}
+
 	/**
-	 * Este método se encarga de mostrar los detalles de un evento destacado, obteniendo la información necesaria del evento
-	 * y del usuario actual y luego pasándola a una vista llamada "detallesDestacado".
+	 * Este método se encarga de mostrar los detalles de un evento destacado,
+	 * obteniendo la información necesaria del evento y del usuario actual y luego
+	 * pasándola a una vista llamada "detallesDestacado".
 	 * 
-	 * @param idEvento		Es el indentificador propio del evento
-	 * @param model			Añade el objeto evento al modelo con el nombre "evento". Permitiendo que la información del evento
-	 * 						esté disponible en la vista.
-	 * @param usuario		Agrega el objeto usu (información del usuario) al modelo con el nombre "usuario".
-	 * 						permitiendo que la información del usuario esté disponible en la vista.
-	 * @return				Devuelve a la vista que se mostrará después de ejecutar el método. En este caso,
-	 *  			 		vista se llama "detallesDestacado".
+	 * @param idEvento Es el indentificador propio del evento
+	 * @param model    Añade el objeto evento al modelo con el nombre "evento".
+	 *                 Permitiendo que la información del evento esté disponible en
+	 *                 la vista.
+	 * @param usuario  Agrega el objeto usu (información del usuario) al modelo con
+	 *                 el nombre "usuario". permitiendo que la información del
+	 *                 usuario esté disponible en la vista.
+	 * @return Devuelve a la vista que se mostrará después de ejecutar el método. En
+	 *         este caso, vista se llama "detallesDestacado".
 	 */
 	@GetMapping("/eventosDestacados/verDetalles/{id}")
-	public String detallesEventosDestacados (@PathVariable ("id") int idEvento, Model model, Authentication auth) {
-		 Evento evento = eDao.verUnEvento(idEvento);
-		    auth = SecurityContextHolder.getContext().getAuthentication();
-	        String usu = auth.getName();
+	public String detallesEventosDestacados(@PathVariable("id") int idEvento, Model model, Authentication auth) {
+		Evento evento = eDao.verUnEvento(idEvento);
+		auth = SecurityContextHolder.getContext().getAuthentication();
+		String usu = auth.getName();
 
-		    int cantidadDisponible = evento.getAforoMaximo() - rDao.cantReservas(idEvento);
-		    int limiteMaximo = 10 - rDao.rvasPorUsuarioYEvento(idEvento, usu);
+		int cantidadDisponible = evento.getAforoMaximo() - rDao.cantReservas(idEvento);
+		int limiteMaximo = 10 - rDao.rvasPorUsuarioYEvento(idEvento, usu);
 
-		    model.addAttribute("evento", evento);
-		    model.addAttribute("usuario", usu);
-		    model.addAttribute("cantidad", cantidadDisponible);
-		    model.addAttribute("limiteMaximo", limiteMaximo);
+		model.addAttribute("evento", evento);
+		model.addAttribute("usuario", usu);
+		model.addAttribute("cantidad", cantidadDisponible);
+		model.addAttribute("limiteMaximo", limiteMaximo);
 		return "detallesDestacado";
 	}
-	
-	
-	
-	
-	
+
 	/**
-	 * Este método se utiliza para realizar una reserva después de visualizar los detalles de un evento destacado. 
-	 * Agrega un mensaje de éxito como atributo flash y redirige al usuario a la página de "misReservas".
+	 * Este método se utiliza para realizar una reserva después de visualizar los
+	 * detalles de un evento destacado. Agrega un mensaje de éxito como atributo
+	 * flash y redirige al usuario a la página de "misReservas".
 	 * 
-	 * @param ratt			Agrega un mensaje de éxito como atributo flash ( Reserva realizada con éxito )
-	 * @param idEvento		Identificador del evento
-	 * @param usuario		Objeto con la información del usuario
-	 * @return				Después de realizar la reserva satisfactoriamente, redirige al usuario a la página "/misReservas".
+	 * @param ratt     Agrega un mensaje de éxito como atributo flash ( Reserva
+	 *                 realizada con éxito )
+	 * @param idEvento Identificador del evento
+	 * @param usuario  Objeto con la información del usuario
+	 * @return Después de realizar la reserva satisfactoriamente, redirige al
+	 *         usuario a la página "/misReservas".
 	 */
 	@PostMapping("/eventosDestacados/verDetalles/{id}")
-	public String realizarReservaDestacado(@PathVariable ("id") int idEvento, @RequestParam int cantidad, @RequestParam String observaciones, Authentication auth) {
-        auth = SecurityContextHolder.getContext().getAuthentication();
-        String username = auth.getName();
-        Evento evento = eDao.verUnEvento(idEvento);
-        
-        BigDecimal precioVenta =  evento.getPrecio().multiply(BigDecimal.valueOf(cantidad));
-        Reserva reserva = new Reserva(cantidad, observaciones, precioVenta, evento, uDao.verUsuario(username));
-        
-        rDao.realizarReserva(reserva);
-        return "redirect:/misReservas";
+	public String realizarReservaDestacado(@PathVariable("id") int idEvento, @RequestParam int cantidad,
+			@RequestParam String observaciones, Authentication auth) {
+		auth = SecurityContextHolder.getContext().getAuthentication();
+		String username = auth.getName();
+		Evento evento = eDao.verUnEvento(idEvento);
+
+		BigDecimal precioVenta = evento.getPrecio().multiply(BigDecimal.valueOf(cantidad));
+		Reserva reserva = new Reserva(cantidad, observaciones, precioVenta, evento, uDao.verUsuario(username));
+
+		rDao.realizarReserva(reserva);
+		return "redirect:/misReservas";
 	}
-	
-	
-	
-	
-	
+
 	/**
-	 * Este método se encarga de mostrar los detalles de un evento activo, obteniendo la información necesaria del evento
-	 * y del usuario actual y luego pasándola a una vista llamada "detallesActivos".
+	 * Este método se encarga de mostrar los detalles de un evento activo,
+	 * obteniendo la información necesaria del evento y del usuario actual y luego
+	 * pasándola a una vista llamada "detallesActivos".
 	 * 
-	 * @param idEvento		Identificador del evento
-	 * @param model			Agrega el objeto evento al modelo con el nombre "evento". Permitiendo que la información del evento
-	 * 						esté disponible en la vista.
-	 * @param usuario		Objeto con la información del usuario
-	 * @return				Devuelve el nombre de la vista que se mostrará después de ejecutar este método en este caso
-	 * 						"detallesActivos".
+	 * @param idEvento Identificador del evento
+	 * @param model    Agrega el objeto evento al modelo con el nombre "evento".
+	 *                 Permitiendo que la información del evento esté disponible en
+	 *                 la vista.
+	 * @param usuario  Objeto con la información del usuario
+	 * @return Devuelve el nombre de la vista que se mostrará después de ejecutar
+	 *         este método en este caso "detallesActivos".
 	 */
 	@GetMapping("/eventosActivos/verDetalles/{id}")
-	public String detallesEventosActivos (@PathVariable ("id") int idEvento, Model model, Authentication auth) {
-		 Evento evento = eDao.verUnEvento(idEvento);
-		    auth = SecurityContextHolder.getContext().getAuthentication();
-	        String usu = auth.getName();
+	public String detallesEventosActivos(@PathVariable("id") int idEvento, Model model, Authentication auth) {
+		Evento evento = eDao.verUnEvento(idEvento);
+		auth = SecurityContextHolder.getContext().getAuthentication();
+		String usu = auth.getName();
 
-		    int cantidadDisponible = evento.getAforoMaximo() - rDao.cantReservas(idEvento);
-		    int limiteMaximo = 10 - rDao.rvasPorUsuarioYEvento(idEvento, usu);
+		int cantidadDisponible = evento.getAforoMaximo() - rDao.cantReservas(idEvento);
+		int limiteMaximo = 10 - rDao.rvasPorUsuarioYEvento(idEvento, usu);
 
-		    model.addAttribute("evento", evento);
-		    model.addAttribute("usuario", usu);
-		    model.addAttribute("cantidad", cantidadDisponible);
-		    model.addAttribute("limiteMaximo", limiteMaximo);
+		model.addAttribute("evento", evento);
+		model.addAttribute("usuario", usu);
+		model.addAttribute("cantidad", cantidadDisponible);
+		model.addAttribute("limiteMaximo", limiteMaximo);
 		return "detallesActivos";
 	}
-	
-	
-	
-	@PostMapping("/eventosActivos/verDetalles/{id}")
-	public String realizarReservaActivos(@PathVariable ("id") int idEvento, @RequestParam int cantidad, @RequestParam String observaciones, Authentication auth) {
-        auth = SecurityContextHolder.getContext().getAuthentication();
-        String username = auth.getName();
-        Evento evento = eDao.verUnEvento(idEvento);
-        
-        BigDecimal precioVenta =  evento.getPrecio().multiply(BigDecimal.valueOf(cantidad));
-        Reserva reserva = new Reserva(cantidad, observaciones, precioVenta, evento, uDao.verUsuario(username));
-        
-        rDao.realizarReserva(reserva);
-        return "redirect:/misReservas";
-	}
-	
 
-	
-	
+	@PostMapping("/eventosActivos/verDetalles/{id}")
+	public String realizarReservaActivos(@PathVariable("id") int idEvento, @RequestParam int cantidad,
+			@RequestParam String observaciones, Authentication auth) {
+		auth = SecurityContextHolder.getContext().getAuthentication();
+		String username = auth.getName();
+		Evento evento = eDao.verUnEvento(idEvento);
+
+		BigDecimal precioVenta = evento.getPrecio().multiply(BigDecimal.valueOf(cantidad));
+		Reserva reserva = new Reserva(cantidad, observaciones, precioVenta, evento, uDao.verUsuario(username));
+
+		rDao.realizarReserva(reserva);
+		return "redirect:/misReservas";
+	}
+
 }
--- src/main/java/eventos/controller/ReservaController.java
+++ src/main/java/eventos/controller/ReservaController.java
@@ -1,6 +1,5 @@
 package eventos.controller;
 
-
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -8,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import eventos.modelo.dao.ReservaDao;
 import eventos.modelo.dao.UsuarioDao;
 import eventos.modelo.entitis.Reserva;
@@ -17,63 +15,63 @@ import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class ReservaController {
-	
+
 	@Autowired
 	private ReservaDao rDao;
-	
+
 	@Autowired
 	private UsuarioDao uDao;
-	
-	
+
 	/**
-	 * Este método maneja solicitudes GET para mostrar las reservas de un usuario. Verifica si hay un usuario autenticado, 
-	 * obtiene la información del usuario y sus reservas asociadas, y luego pasa esta información al modelo antes de redirigir al usuario
-	 * a la vista "misReservas". Si no hay un usuario autenticado, redirige al usuario a la página de inicio de sesión.
+	 * Este método maneja solicitudes GET para mostrar las reservas de un usuario.
+	 * Verifica si hay un usuario autenticado, obtiene la información del usuario y
+	 * sus reservas asociadas, y luego pasa esta información al modelo antes de
+	 * redirigir al usuario a la vista "misReservas". Si no hay un usuario
+	 * autenticado, redirige al usuario a la página de inicio de sesión.
 	 * 
-	 * @param model			Para pasar datos a la vista
-	 * @param aut			Objeto para manejar la información de autenticación del usuario
-	 * @param misesion		Objeto para controlar la sesión.
-	 * @param usuario		Objeto usuario con la información del mismo.
-	 * @return				Devuelve el nombre de la vista que se mostrará después de ejecutar este método "misReservas".
-	 * 						En caso de que el usuario no este autentificado redirige a la página ("/login").
+	 * @param model    Para pasar datos a la vista
+	 * @param aut      Objeto para manejar la información de autenticación del
+	 *                 usuario
+	 * @param misesion Objeto para controlar la sesión.
+	 * @param usuario  Objeto usuario con la información del mismo.
+	 * @return Devuelve el nombre de la vista que se mostrará después de ejecutar
+	 *         este método "misReservas". En caso de que el usuario no este
+	 *         autentificado redirige a la página ("/login").
 	 */
 	@GetMapping("/misReservas")
-	public String verMisReservas(Model model, Authentication aut, HttpSession misesion, Usuario usuario) {
-	    if (aut != null && aut.isAuthenticated()) {
-	        String nombreUsuario = aut.getName();
+	public String verMisReservas(Model model, Authentication aut, HttpSession misesion) {
+		if (aut != null && aut.isAuthenticated()) {
+			String nombreUsuario = aut.getName();
 
-	        Usuario user = uDao.verUsuario(nombreUsuario);
-	        List<Reserva> reservas = rDao.verReservasPorUsuario(user);
-	        model.addAttribute("reservas", reservas);
-	        return "misReservas";
-	    } else {
-	        return "redirect:/login";
-	    }
+			Usuario user = uDao.verUsuario(nombreUsuario);
+			List<Reserva> reservas = rDao.verReservasPorUsuario(user);
+			model.addAttribute("reservas", reservas);
+			return "misReservas";
+		} else {
+			return "redirect:/login";
+		}
 	}
-	
 
-	
 	/**
-	 * Este método maneja solicitudes GET para eliminar una reserva específica. Intenta eliminar la reserva con el identificador proporcionado, 
-	 * agrega un mensaje al modelo según el resultado y redirige al usuario de vuelta a la página de sus reservas.
+	 * Este método maneja solicitudes GET para eliminar una reserva específica.
+	 * Intenta eliminar la reserva con el identificador proporcionado, agrega un
+	 * mensaje al modelo según el resultado y redirige al usuario de vuelta a la
+	 * página de sus reservas.
 	 * 
-	 * @param idReserva		Identificador de la reserva a eliminar
-	 * @param model			Para pasar datos a la vista
-	 * @return				Redirige al usuario a la página de sus reservas después de eliminar la reserva.
-	 * 						En caso de no poder eliminar la reserva, lo indica con un mensaje ("No ha podido eliminar la reserva").
+	 * @param idReserva Identificador de la reserva a eliminar
+	 * @param model     Para pasar datos a la vista
+	 * @return Redirige al usuario a la página de sus reservas después de eliminar
+	 *         la reserva. En caso de no poder eliminar la reserva, lo indica con un
+	 *         mensaje ("No ha podido eliminar la reserva").
 	 */
 	@GetMapping("/misReservas/eliminar/{id}")
-	public String eliminarReserva(@PathVariable ("id") int idReserva, Model model) {
-		if(rDao.eliminarReserva(idReserva) == 1) {
-		model.addAttribute("mensaje", "Se ha eliminado la reserva");
-		return "redirect:/misReservas";
-		}else
+	public String eliminarReserva(@PathVariable("id") int idReserva, Model model) {
+		if (rDao.eliminarReserva(idReserva) == 1) {
+			model.addAttribute("mensaje", "Se ha eliminado la reserva");
+			return "redirect:/misReservas";
+		} else
 			model.addAttribute("mensaje", "No ha podido eliminar la reserva");
 		return "redirect:/misReservas";
 	}
-	
-	
-	
-	
-    
+
 }
